fb7dbf4062d2ae77e77ea594c9d5350ce752641e
User <User@USER-PC>
2019-06-08 20:44:56 +0200

    RScada

--- AdvancedScada.HMI/Properties/AssemblyInfo.cs
+++ AdvancedScada.HMI/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdvancedScada.HMI")]
+[assembly: AssemblyTitle("AdvancedScada.Scada")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdvancedScada.HMI")]
+[assembly: AssemblyProduct("AdvancedScada.Scada")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
